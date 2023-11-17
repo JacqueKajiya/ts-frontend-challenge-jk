1c34111b39bbc5041d2771e8d5c807f61d0d9d4d
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-11-17 20:03:54 -0300

    feat: added global style and removed images

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,29 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    list-style: none;
+    font-family: 'Lato', sans-serif;
+  }
+
+  button{
+    cursor: pointer;
+  }
+
+  body{
+    padding: 0 1rem;
+  }
+
+  :root{
+    --color-primary: #2D3748;
+    --color-secondary: #4A5568;
+
+    --color-text-white: #F7FAFC;
+    --color-input-placeholder: #A0AEC0;
+
+    --negative: #F56565;
+  }
+`;
